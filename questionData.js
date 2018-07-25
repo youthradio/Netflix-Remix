d76d05e2272b70fc644f28a928b1428857cfd1e4
Radames Ajna <radamajna@gmail.com>
2018-07-24 21:45:52 -0500

    fix identation

--- questionData.js
+++ questionData.js
@@ -1,32 +1,41 @@
-var funFactGenre = {"fact1":"Lorem ipsum dolor sit amet, consectetuer adipiscingelit. Aenean commodo ligula eget dolor. Aenean massa.",
-"fact2":"Lorem ipsum dolor sit amet, consectetuer adipiscingelit. Aenean commodo ligula eget dolor. Aenean massa.",
-"fact3":"Lorem ipsum dolor sit amet, consectetuer adipiscingelit. Aenean commodo ligula eget dolor. Aenean massa."};
+var funFactGenre = {
+  "fact1": "Lorem ipsum dolor sit amet, consectetuer adipiscingelit. Aenean commodo ligula eget dolor. Aenean massa.",
+  "fact2": "Lorem ipsum dolor sit amet, consectetuer adipiscingelit. Aenean commodo ligula eget dolor. Aenean massa.",
+  "fact3": "Lorem ipsum dolor sit amet, consectetuer adipiscingelit. Aenean commodo ligula eget dolor. Aenean massa."
+};
 
-var funFactGender = {"fact1":"Lorem ipsum dolor sit amet, consectetuer adipiscingelit. Aenean commodo ligula eget dolor. Aenean massa.",
-"fact2":"Lorem ipsum dolor sit amet, consectetuer adipiscingelit. Aenean commodo ligula eget dolor. Aenean massa.",
-"fact3":"Lorem ipsum dolor sit amet, consectetuer adipiscingelit. Aenean commodo ligula eget dolor. Aenean massa."};
+var funFactGender = {
+  "fact1": "Lorem ipsum dolor sit amet, consectetuer adipiscingelit. Aenean commodo ligula eget dolor. Aenean massa.",
+  "fact2": "Lorem ipsum dolor sit amet, consectetuer adipiscingelit. Aenean commodo ligula eget dolor. Aenean massa.",
+  "fact3": "Lorem ipsum dolor sit amet, consectetuer adipiscingelit. Aenean commodo ligula eget dolor. Aenean massa."
+};
 
 var dataPack = [
 
-{"question":"What’s your favorite type of movie?",
-options: {
-	"one":"Action", 
-	"two":"Comedy", 
-	"three":"Romance"
-}},
-{"question":"Should the lead be…",
-options: {
-	"one":"Female", 
-	"two":"Male", 
-	"three":"Nonbinary"
-}},
+  {
+    "question": "What’s your favorite type of movie?",
+    options: {
+      "one": "Action",
+      "two": "Comedy",
+      "three": "Romance"
+    }
+  },
+  {
+    "question": "Should the lead be…",
+    options: {
+      "one": "Female",
+      "two": "Male",
+      "three": "Nonbinary"
+    }
+  },
 
-{
-"question":"And looks like…",
-options:{
-	"one":"Black", 
-	"two":"Latino", 
-	"three":"Asian"
-}},
+  {
+    "question": "And looks like…",
+    options: {
+      "one": "Black",
+      "two": "Latino",
+      "three": "Asian"
+    }
+  },
 
 ]
