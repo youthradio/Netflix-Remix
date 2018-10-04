e99b8cf46a2049ff98e59469b2333fad011b06b4
Asha Richardson <asha@youthradio.org>
2018-10-03 18:22:54 -0700

    add m-4 to buttons

--- js/main.js
+++ js/main.js
@@ -29,7 +29,7 @@ function nextPage(npage) {
             //
             // image.setAttribute('xlink:href', obj.url)
             // opt.dataset.key = obj.label
-            button.classList.add('button-style')
+            button.classList.add('button-style','m-4')
             button.dataset.key = obj.label
             button.innerHTML = obj.label
             button.addEventListener('click', event => {
